bdbe2b9f8e7a2cb17ab0688c4401bf6c22ae1f06
AR-DThon <160032870+AR-DThon@users.noreply.github.com>
2024-02-17 15:46:52 +0300

    Add files via upload

---
+++ env.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = os.getenv("API_ID", "15179868").strip()
+API_HASH = os.getenv("API_HASH", "5eed1d89e639551bd74d736037ebd4f9").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6267525819:AAHK3H2kKNrLrxb5SsQ_KaYlr7bFaxpcbo8").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/SHA_HM1")
+
+if not API_ID:
+    print("No API_ID found. Exiting...")
+    raise SystemExit
+if not API_HASH:
+    print("No API_HASH found. Exiting...")
+    raise SystemExit
+if not BOT_TOKEN:
+    print("No BOT_TOKEN found. Exiting...")
+    raise SystemExit
+if not DATABASE_URL:
+    print("No DATABASE_URL found. Exiting...")
+    raise SystemExit
+
+try:
+    API_ID = int(API_ID)
+except ValueError:
+    print("API_ID is not a valid integer. Exiting...")
+    raise SystemExit
+
+if "postgres" in DATABASE_URL and "postgresql" not in DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
